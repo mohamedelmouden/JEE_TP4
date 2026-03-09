112316d07f4bf64d9318b5c0b41f35dda5543f00
mohamed el mouden <m.elmouden1908@uca.ac.ma>
2026-03-09 22:52:56 +0000

    commit1

---
+++ src/main/java/entities/Machine.java
@@ -0,0 +1,70 @@
+package entities;
+// on va d'abord créer le package entities
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@NamedNativeQuery(name = "findBetweenDateNative", query = "select * from machine where dateAchat between :d1 and :d2", resultClass = Machine.class)
+@NamedQuery(name = "findBetweenDate", query = "from Machine where dateAchat between :d1 and :d2")
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String ref;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateAchat;
+
+    @ManyToOne
+    private Salle salle;
+
+    public Machine() {
+    }
+
+    public Machine(String ref, Date dateAchat, Salle salle) {
+        this.ref = ref;
+        this.dateAchat = dateAchat;
+        this.salle = salle;
+    }
+// le constructeur demmandé
+    public Machine(int id, String ref, Date dateAchat, Salle salle) {
+        this.id = id;
+        this.ref = ref;
+        this.dateAchat = dateAchat;
+        this.salle = salle;
+    }
+    // voici le constructeur pae defaut et le constructeur parametré
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+}
---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,54 @@
+package entities;
+// dans le m package que Machine
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+    public  Salle() {
+    }
+
+    public Salle(String code) {
+        this.code = code;
+    }
+// le constrecteur demmandé
+    public Salle(int id, String code, List<Machine> machines) {
+        this.id = id;
+        this.code = code;
+        this.machines = machines;
+    }
+    // voici les deux constructeurs
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
